17ce759fcac1e450c867eafa1ff971fe9968bbd8
Jeanmichel7 <jeanmichel.rasser@laposte.net>
2023-03-28 20:45:48 +0200

    config connection db

---
+++ back/config/typeorm.config.ts
@@ -0,0 +1,19 @@
+// import { Module } from '@nestjs/common';
+// import { ConfigModule, ConfigService } from '@nestjs/config';
+// import { TypeOrmModuleOptions } from "@nestjs/typeorm";
+
+
+// export const typeOrmConfig: TypeOrmModuleOptions = {
+//     imports: [ConfigModule],
+//     useFactory: (configService: ConfigService) => ({
+//       type: 'postgres',
+//       host: configService.get('DB_HOST'),
+//       port: +configService.get<number>('DB_PORT'),
+//       username: configService.get('DB_USERNAME'),
+//       password: configService.get('DB_PASSWORD'),
+//       database: configService.get('DB_NAME'),
+//       entities: [],
+//       synchronize: true,
+//     }),
+//     inject: [ConfigService],
+// }
