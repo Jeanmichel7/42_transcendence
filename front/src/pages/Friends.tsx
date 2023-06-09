0d21eba8d66733a4c7627c0940029f35a2de2eb9
Jeanmichel7 <jeanmichel.rasser@laposte.net>
2023-06-09 03:03:04 +0200

    friends pages: badge menu

--- front/src/pages/Friends.tsx
+++ front/src/pages/Friends.tsx
@@ -1,13 +1,15 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import WaitingAcceptRequest from '../components/Friends/WaitingAcceptRequest';
 import WaitingRequestSent from '../components/Friends/WaitingRequestSent';
 import AllFriends from '../components/Friends/AllFriends';
 import BlockedUser from '../components/Friends/BlockedUser';
 
-import { AppBar, Box, Tab, Tabs, Toolbar, Typography } from '@mui/material';
+import { AppBar, Badge, Box, Divider, Tab, Tabs, Toolbar, Typography } from '@mui/material';
 import OnLineFriends from '../components/Friends/OnLineFriends';
 import AddFriends from '../components/Friends/AddFriends';
+import { useSelector } from 'react-redux';
+import { RootState } from '../store';
 
 interface TabPanelProps {
   children?: React.ReactNode;
@@ -38,14 +40,47 @@ function TabPanel(props: TabPanelProps) {
 
 export default function FriendsPage() {
   const [value, setValue] = useState(0);
+  const [onlineCount, setOnlineCount] = useState<number>(0);
+  const [friendsCount, setFriendsCount] = useState<number>(0);
+  const [blockedCount, setBlockedCount] = useState<number>(0);
+  const [waitingRequestCount, setWaitingRequestCount] = useState<number>(0);
+  const [waitingSentCount, setWaitingSentCount] = useState<number>(0);
 
+  const userData = useSelector((state: RootState) => state.user.userData);
+
+  useEffect(() => {
+    if (userData.friends) {
+      setFriendsCount(userData.friends.length);
+      setOnlineCount(userData.friends.filter((f) => f.status !== 'offline').length);
+    }
+  }, [userData.friends]);
+
+  useEffect(() => {
+    if (userData.waitingFriendsRequestReceived)
+      setWaitingRequestCount(userData.waitingFriendsRequestReceived.length);
+  }, [userData.waitingFriendsRequestReceived]);
+
+  useEffect(() => {
+    if (userData.waitingFriendsRequestSent)
+      setWaitingSentCount(userData.waitingFriendsRequestSent.length);
+  }, [userData.waitingFriendsRequestSent]);
+
+  useEffect(() => {
+    if (userData.userBlocked)
+      setBlockedCount(userData.userBlocked.length);
+  }, [userData.userBlocked]);
+  
   const handleChange = (event: React.SyntheticEvent, newValue: number) => {
     setValue(newValue);
   };
 
+  if (!userData) {
+    return <p>Loading...</p>;
+  }
+
   return (
     <>
-      <Box sx={{ bgcolor: 'background.paper', width: 'auto' }}>
+      <Box sx={{ bgcolor: 'background.paper' }}>
         <AppBar position="static">
           <Toolbar>
             <Typography variant="h6" component="div">
@@ -54,41 +89,45 @@ export default function FriendsPage() {
             <Tabs
               value={value}
               onChange={handleChange}
-              indicatorColor="secondary"
+              indicatorColor={'secondary'}
+              // selectionFollowsFocus={true}
               textColor="inherit"
-              variant="fullWidth"
-              aria-label="full width tabs example"
+              // variant="fullWidth"
+              variant="scrollable"
+              scrollButtons={true}
+              // visibleScrollbar={true}
+              aria-label="nav friends"
             >
-              <Tab label="Online" />
-              <Tab label="All Friend" />
-              <Tab label="Waiting accept " />
-              <Tab label="Waiting send" />
-              <Tab label="Blocked" />
+              <Tab label= {'Online (' + onlineCount + ')'}/>
+              <Tab label={'All Friends (' + friendsCount + ')'} />
+              <Tab label={
+                <Badge 
+                  color="secondary" 
+                  badgeContent={waitingRequestCount}
+                >
+                  Waiting received
+                </Badge>
+              }/>
+              <Tab label={
+                <Badge 
+                  color="secondary" 
+                  badgeContent={waitingSentCount}
+                >
+                  Waiting sent
+                </Badge>
+              }/>
+              <Tab label={'Blocked (' + blockedCount + ')'} />
               <Tab label="Add Friend" />
             </Tabs>
           </Toolbar>
-
         </AppBar>
 
-        <TabPanel value={value} index={0} >
-          <OnLineFriends />
-        </TabPanel>
-        <TabPanel value={value} index={1} >
-          <AllFriends />
-        </TabPanel>
-        <TabPanel value={value} index={2} >
-          <WaitingAcceptRequest />
-        </TabPanel>
-        <TabPanel value={value} index={3} >
-          <WaitingRequestSent />
-        </TabPanel>
-        <TabPanel value={value} index={4} >
-          <BlockedUser />
-        </TabPanel>
-        <TabPanel value={value} index={5} >
-          <AddFriends />
-        </TabPanel>
-
+        <TabPanel value={value} index={0}> <OnLineFriends /> </TabPanel>
+        <TabPanel value={value} index={1}> <AllFriends /> </TabPanel>
+        <TabPanel value={value} index={2}> <WaitingAcceptRequest /> </TabPanel>
+        <TabPanel value={value} index={3}> <WaitingRequestSent /> </TabPanel>
+        <TabPanel value={value} index={4}> <BlockedUser /> </TabPanel>
+        <TabPanel value={value} index={5}> <AddFriends /> </TabPanel>
       </Box>
     </>
   );
